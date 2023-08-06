fa1b9a3a8a47cd7d4681ea1a02ea2e7848fb20ea
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-08-06 19:22:30 -0300

    atualizado

--- src/main/java/br/edu/ifba/saj/ads/poo/App.java
+++ src/main/java/br/edu/ifba/saj/ads/poo/App.java
@@ -2,22 +2,37 @@ package br.edu.ifba.saj.ads.poo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
+    private static Scene scene;
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.Load(getClass().getResource("primary.fxml"));
-        prymaryStage.setTitle ();
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 
 }
