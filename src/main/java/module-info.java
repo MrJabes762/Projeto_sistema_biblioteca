8ee50e8164f5b1d9b463a4e47ddef5a1b6483979
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-08-06 19:49:55 -0300

    Refazer projeto

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module br.edu.ifba.saj.ads.poo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens br.edu.ifba.saj.ads.poo to javafx.fxml;
-    exports br.edu.ifba.saj.ads.poo;
-}
