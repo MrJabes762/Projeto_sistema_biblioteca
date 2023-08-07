47d8ef94a1173c559775a1ab0466d8aadd704bea
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-08-06 22:20:52 -0300

    Construção da tela inicial

--- src/main/java/sistema/Main.java
+++ src/main/java/sistema/Main.java
@@ -1,7 +1,32 @@
 package sistema;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class App extends Application{
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("apresentacao"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch();
     }
 }
---
+++ src/main/java/sistema/app/Conttrollers/TelaInicial/TelaInicialController.java
@@ -0,0 +1,17 @@
+package sistema.app.Conttrollers.TelaInicial;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TelaInicialController {
+
+    @FXML
+    private Button btnEntrar;
+
+    @FXML
+    void entrarNaPaginaInicial(ActionEvent event) {
+
+    }
+
+}
