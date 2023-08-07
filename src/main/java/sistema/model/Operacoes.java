3edaefbd857cae152aa138d0dc0faa508dc98b70
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-08-06 21:50:04 -0300

    Adição de classes implementadas

---
+++ src/main/java/sistema/model/Operacoes.java
@@ -0,0 +1,6 @@
+package sistema.model;
+
+public interface Operacoes {
+    public abstract void adicionarLivronaMochila (Livro2 livro);
+    public abstract Livro2 removerLivrodaMochila (String codigo);
+}
