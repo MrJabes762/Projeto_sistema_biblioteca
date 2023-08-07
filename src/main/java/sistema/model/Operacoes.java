b7f1b31edf6b04a5356446d97e379ac504654daf
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-08-07 12:21:07 -0300

    Refazer o projeto

--- src/main/java/sistema/model/Operacoes.java
+++
@@ -1,6 +0,0 @@
-package sistema.model;
-
-public interface Operacoes {
-    public abstract void adicionarLivronaMochila (Livro2 livro);
-    public abstract Livro2 removerLivrodaMochila (String codigo);
-}
